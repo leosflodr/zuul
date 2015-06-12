d5942b7d5dbeaf197064c45a33344b0b2bac9931
Wayne <wayne@puppetlabs.com>
2015-06-12 16:56:30 -0700

    Add basic Github Zuul Reporter.
    Change-Id: I3c34bb1354adb7c360e173c227f00bf987b7d30e
    Co-Authored-By: Jan Hruban <jan.hruban@gooddata.com>

--- zuul/layoutvalidator.py
+++ zuul/layoutvalidator.py
@@ -170,6 +170,7 @@ class LayoutSchema(object):
                 'gerrit': 'zuul.reporter.gerrit',
                 'smtp': 'zuul.reporter.smtp',
                 'sql': 'zuul.reporter.sql',
+                'github': 'zuul.reporter.github',
             },
         }
         standard_drivers = {
@@ -302,6 +303,7 @@ class LayoutValidator(object):
             'reporter': {
                 'gerrit': 'zuul.reporter.gerrit',
                 'smtp': 'zuul.reporter.smtp',
+                'github': 'zuul.reporter.github',
             },
         }
         standard_drivers = {
